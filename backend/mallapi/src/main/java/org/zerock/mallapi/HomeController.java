36c80a7ca767d0645feeb407b1f8a9774f240c0e
sehyun <sh000917@gmail.com>
2024-11-29 13:53:51 +0900

    no message

---
+++ backend/mallapi/src/main/java/org/zerock/mallapi/HomeController.java
@@ -0,0 +1,12 @@
+package org.zerock.mallapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Mall API!";
+    }
+}
